cac0b02acb95a9b362cde3e622ee8d16a97fe22c
unknown <rrafal90@gmail.com>
2023-02-16 22:36:08 +0100

    Rozwiazanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int evenNumber = 30;
+        int oddNumber = 2137;
+        BasicMath  basicMath = new BasicMath();
+        System.out.println("Czy cyfra " + evenNumber + " jest liczbą parzystą?: " + basicMath.isEven(evenNumber));
+        System.out.println("Czy cyfra " + oddNumber + " jest liczbą nieparzystą?: " + basicMath.isOdd(oddNumber));
+        System.out.println("Pole koła o promieniu " + oddNumber + " wynosi: " + basicMath.circleField(oddNumber));
+        System.out.println(evenNumber + " podniesione do kwadratu równa się " + basicMath.power(evenNumber));
     }
 }
